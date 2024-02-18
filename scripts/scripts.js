f7a7165a31b2a9f65bfb4dc52fafbade813e3e05
imranmashoud <aaborjona@gmail.com>
2024-02-18 17:44:37 +0600

    Scroll function added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,7 @@
+document.getElementById("scrollButton").addEventListener("click", function () {
+  // Get the target element
+  var targetElement = document.getElementById("targetElement");
 
+  // Scroll to the target element
+  targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+});
