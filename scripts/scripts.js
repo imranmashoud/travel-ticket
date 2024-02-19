4491499b7ca87b59079a9b5c640a9ac9a47087cb
imranmashoud <aaborjona@gmail.com>
2024-02-19 07:50:17 +0600

    Discount coupon 15% is added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,6 +9,7 @@ document.getElementById("scrollButton").addEventListener("click", function () {
 const allBtn = document.getElementsByClassName("seat-btn");
 
 let count = 0;
+let ticketPrice = 550;
 let totalPrice = 0;
 
 for (const btn of allBtn) {
@@ -18,7 +19,7 @@ for (const btn of allBtn) {
     document.getElementById("available-seat").innerText = 40 - count;
     element.target.style.backgroundColor = "#1DD100";
     const getSeat = element.target.innerText;
-    let ticketPrice = parseInt(document.getElementById("seat-price").innerText);
+    // let ticketPrice = parseInt(document.getElementById("seat-price").innerText);
     // console.log(typeof ticketPrice);
 
     const parentContainer = document.getElementById("seat-number");
@@ -42,3 +43,27 @@ for (const btn of allBtn) {
     document.getElementById("total-price").innerText = totalPrice;
   });
 }
+
+const btn = document.getElementById("apply-btn");
+btn.addEventListener("click", function () {
+  // console.log("clicked");
+
+  const couponElement = document.getElementById("input-discount").value;
+  const couponCode = couponElement.split(" ").join("").toUpperCase();
+  // console.log(couponCode);
+  if (couponCode === "NEW15") {
+    const discountContainer = document.getElementById("discounted-amount");
+    const disAmount = totalPrice * 0.15;
+    const disDiv = document.createElement("div");
+    const p1 = document.createElement("p");
+    p1.innerText = "Discount amount";
+    const p2 = document.createElement("p");
+    p2.innerText = disAmount;
+    disDiv.appendChild(p1);
+    disDiv.appendChild(p2);
+    discountContainer.appendChild(disDiv);
+    disDiv.classList.add("flex", "justify-between");
+  } else {
+    alert("Invalid coupon");
+  }
+});
